a8259732bfdcd99795ffd0f0ddc0c0b2e59863c6
Muayad Bakhtan <pmuayad@gmail.com>
2020-05-19 13:13:37 -0400

    first submition

---
+++ main.js
@@ -0,0 +1,34 @@
+let TotaleEl = document.querySelector("#totale")
+let inputEl = document.querySelector("#input")
+let submitButton = document.querySelector("#submit")
+let totalshow=document.querySelector("#show-all-rolls")
+let allRolls=document.querySelector("#all_roll")
+var dieRolls = [];
+submitButton.addEventListener("click", function () {
+    //console.log(inputEl, TotaleEl, submitButton, dicerollElm)
+
+    let userinput = inputEl.value
+    let count = 0
+    for (count = 0; count < userinput; count++) {
+        currentRoll = Math.floor(Math.random() * 6 + 1)
+        dieRolls.push(currentRoll)
+        console.log(dieRolls)
+    }
+})
+
+totalshow.addEventListener("click", function () {
+   // let totalRoll = 0
+    for (let count1 = 0; count1 < dieRolls.length; count1++) {
+        let numberRolled = dieRolls[count1]
+        const newDiceString = '<li class="dice">' + numberRolled + "</li>";
+        allRolls.innerHTML+=newDiceString
+    }
+    console.log(dieRolls)
+
+
+})
+
+
+
+
+
